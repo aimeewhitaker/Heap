d005a77a4367a44bd286a7d74b321897a978bb64
aimeewhitaker <43830366+aimeewhitaker@users.noreply.github.com>
2019-04-05 10:15:59 +0200

    Add files via upload

---
+++ tests/alloc2.c
@@ -0,0 +1,14 @@
+/*a few allocations in multiples of 4 bytes*/
+#include <assert.h>
+#include <stdlib.h>
+#include "mem.h"
+
+int main() {
+   assert(Init_Mem(4096) == 0);
+   assert(Alloc_Mem(4) != NULL);
+   assert(Alloc_Mem(8) != NULL);
+   assert(Alloc_Mem(16) != NULL);
+   assert(Alloc_Mem(24) != NULL);
+   Dump_Mem();
+   exit(0);
+}
